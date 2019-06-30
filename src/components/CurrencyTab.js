5e6897b9ee7a4752294f4dd24b3f88ecd33cf7c6
VlTre <vladdd3@gmail.com>
2019-06-30 21:25:22 +0300

    add highcharts

--- src/components/CurrencyTab.js
+++ src/components/CurrencyTab.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table, Card  } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 
 class CurrencyTab extends Component{
     constructor(props){
@@ -88,16 +88,7 @@ class CurrencyTab extends Component{
                     </tr>
                     </tbody>
                 </Table>
-                <Card bg="dark" text="white" style={{ width: '18rem' }}>
-                    <Card.Header>Header</Card.Header>
-                    <Card.Body>
-                        <Card.Title>Dark Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk
-                            of the card's content.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+
 
             </div>
         );
